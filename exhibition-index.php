e44bedaf7035aaf21ff627cbf22d824dbefcb209
brennnnan <brennan.mackay@sbcglobal.net>
2018-05-19 17:44:56 -0700

    removed some stuff

--- exhibition-index.php
+++ exhibition-index.php
@@ -1,7 +1,6 @@
 <?php /* Template Name: exhibition-index */ ?>
   <?php
 /**
- * The front page template file
  *
  * If the user has selected a static page for their homepage, this is what will
  * appear.
--- header.php
+++ header.php
@@ -25,17 +25,7 @@
 <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css" type="text/css" media="all">
-
-<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/teachers/assets/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/teachers/assets/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/teachers/assets/favicon/favicon-16x16.png">
-<link rel="manifest" href="/wp-content/themes/teachers/assets/favicon/manifest.json">
-<link rel="mask-icon" href="/wp-content/themes/teachers/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
-<link rel="shortcut icon" href="/wp-content/themes/teachers/assets/favicon/favicon.ico">
-<meta name="msapplication-config" content="/wp-content/themes/teachers/assets/favicon/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
-
-<script type="text/javascript" src="https://commonwealthandcouncil.com/wp-content/themes/commonwealth/js/materialize.min.js"></script>
 
 </head>
 
